fbb6e970eae0833f2682ada87ce20bfd1a9bfc0e
dokibacsi <kvalcsi02@gmail.com>
2023-02-23 16:02:26 +0100

    metodusokba szedes

--- MetodGyak.java
+++ MetodGyak.java
@@ -1,24 +1,14 @@
 package metodgyak;
 public class MetodGyak {
     public static void main(String[] args) {
-       kiir("Az első 10 szám összege: "+ elso10SzamOsszege());
-       kiir("\n");
-       int szam1, szam2, osszeg, osszeg2;
-       szam1 = 16;
-       szam2 = 22;
-       int szam3 = 20;
-       int szam4 = 18;
-       osszeg = osszead(szam1, szam2);
-       osszeg2 = osszead(szam3, szam4);
-       int negySzamOsszege = osszead(osszeg, osszeg2);
-       kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
-       kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, negySzamOsszege));
-       // gyökvonás
-       double gyok, gyokosszeg;
-       gyok = 0;
-       int haromszam = osszead(szam3, osszead(szam1, szam2));
-       gyokosszeg = Math.sqrt(haromszam);
-       kiir("%d + %d + %d gyöke: %s\n", szam1, szam2, szam3, gyokosszeg);
+       feladatok();
+    }
+    
+    public static void feladatok() {
+        tizSzamOsszege();
+        ketSzamOsszege();
+        negySzamOsszege();
+        haromszamGyokVonasa();
     }
     
     private static int elso10SzamOsszege() {
@@ -38,4 +28,41 @@ public class MetodGyak {
         System.out.print(szoveg);
     }
     
+    private static void tizSzamOsszege(){
+        kiir("Az első 10 szám összege: "+ elso10SzamOsszege());
+    }
+    
+    private static void ketSzamOsszege(){
+       int szam1, szam2, osszeg;
+       szam1 = 16;
+       szam2 = 22;
+       osszeg = osszead(szam1, szam2);
+       kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
+    }
+    
+    private static void negySzamOsszege(){
+       int szam1, szam2, osszeg, osszeg2;
+       szam1 = 16;
+       szam2 = 22;
+       int szam3 = 20;
+       int szam4 = 18;
+       osszeg = osszead(szam1, szam2);
+       osszeg2 = osszead(szam3, szam4);
+       int negySzamOsszege = osszead(osszeg, osszeg2);
+       kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, negySzamOsszege));
+    }
+    
+    private static void haromszamGyokVonasa(){
+       int szam1, szam2, osszeg, osszeg2;
+       szam1 = 16;
+       szam2 = 22;
+       int szam3 = 20;
+       int szam4 = 18;
+       double gyokosszeg;
+       int haromszam = osszead(szam3, osszead(szam1, szam2));
+       gyokosszeg = Math.sqrt(haromszam);
+       kiir("%d + %d + %d gyöke: ", szam1, szam2, szam3);
+       String kimenet = String.format("%s %4.f\n", gyokosszeg);
+       kiir(kimenet);
+    }
 }
