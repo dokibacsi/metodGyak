0a3bd9b8e65ace7501e8eed164565fe999ac9c3e
dokibacsi <kvalcsi02@gmail.com>
2023-02-23 15:52:00 +0100

    gyokvonas szamolasa

--- MetodGyak.java
+++ MetodGyak.java
@@ -4,14 +4,21 @@ public class MetodGyak {
        kiir("Az első 10 szám összege: "+ elso10SzamOsszege());
        kiir("\n");
        int szam1, szam2, osszeg, osszeg2;
-       szam1 = 4;
-       szam2 = 8;
-       int szam3 = 28;
-       int szam4 = 2;
+       szam1 = 16;
+       szam2 = 22;
+       int szam3 = 20;
+       int szam4 = 18;
        osszeg = osszead(szam1, szam2);
        osszeg2 = osszead(szam3, szam4);
-       int negySzamOsszege = osszeg + osszeg2;
+       int negySzamOsszege = osszead(osszeg, osszeg2);
        kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
+       kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, negySzamOsszege));
+       // gyökvonás
+       double gyok, gyokosszeg;
+       gyok = 0;
+       int haromszam = osszead(szam3, osszead(szam1, szam2));
+       gyokosszeg = Math.sqrt(haromszam);
+       kiir("%d + %d + %d gyöke: %s\n", szam1, szam2, szam3, gyokosszeg);
     }
     
     private static int elso10SzamOsszege() {
